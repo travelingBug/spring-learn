45463e856461770c1d09004ef3b1ddabb99da07f
travelBug <java_shuai@163.com>
2019-11-29 01:10:35 +0800

    mybatis使用示例

---
+++ MyBatis_01_HelloWorld/src/com/wonders/mybatis/bean/Employee.java
@@ -0,0 +1,37 @@
+package com.wonders.mybatis.bean;
+
+public class Employee {
+	
+	private Integer id;
+	private String lastName;
+	private String gender;
+	private String email;
+	public Employee() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
